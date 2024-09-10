acbea5189864ec2ece721f385ab5fc8513a269f5
minhlh <minhlh@haposoft.com>
2024-09-10 11:00:54 +0700

    feat: add not found page

---
+++ features/users/index.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const UserPage = () => (
+  <UserPageStyled>
+    <div>User</div>
+  </UserPageStyled>
+);
+
+const UserPageStyled = styled.div``;
+
+export default UserPage;
