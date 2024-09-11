2446d61f00f9d775d78197236db06bbdeb42fb01
minhlh <hongminhle290@gmail.com>
2024-09-11 21:13:03 +0700

    fix: fix build

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, UnstyledButton, UnstyledButtonProps } from '@mantine/core';
+import { Box, Text, UnstyledButton, UnstyledButtonProps } from '@mantine/core';
 import { IconApps } from '@tabler/icons-react';
 import Link from 'next/link';
 import classes from '@/components/Logo/logo.module.scss';
@@ -13,10 +13,12 @@ type LogoProps = {
 const Logo: React.FC<LogoProps> = ({ width, height, text, href = '', ...others }) => (
   <Box w={width || '100%'}>
     <UnstyledButton component={Link} href={href} className={classes.logo} {...others}>
-      <Flex justify="flex-start" align="center" gap={12}>
+      <div
+        style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '16px' }}
+      >
         <IconApps />
         <Text fw={700}>{text}</Text>
-      </Flex>
+      </div>
     </UnstyledButton>
   </Box>
 );
