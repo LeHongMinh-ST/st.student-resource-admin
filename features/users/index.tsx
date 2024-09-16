0e97967eac493124292f8f1b17259e504dc3d472
minhlh <minhlh@haposoft.com>
2024-09-16 11:26:24 +0700

    feat: add compoent action menu

--- features/users/index.tsx
+++ features/users/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import styled from '@emotion/styled';
-import { Button, Container, Text, Stack, Paper, Menu } from '@mantine/core';
+import { Button, Container, Text, Stack, Paper } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { DataTableProps } from 'mantine-datatable';
-import { IconEdit, IconDotsVertical, IconPlus, IconTrash } from '@tabler/icons-react';
+import { IconPlus } from '@tabler/icons-react';
 import { dashboardRoute, userRoute } from '@/routes';
 import { UserListParams, useUserService } from '@/services/userService';
 import { useAuthStore } from '@/utils/recoil/auth/authState';
@@ -19,6 +19,7 @@ import UserNameCellTable from './components/cell/UserNameCellTable';
 import SearchFilter from './components/filters/SearchFilter';
 import { RoleEnum } from '@/enums';
 import RoleFilter from './components/filters/RoleFilter';
+import UserActionMenu from './components/cell/UserActionMenu';
 
 const UserPage = () => {
   const userService = useUserService();
@@ -26,105 +27,76 @@ const UserPage = () => {
   const [meta, setMeta] = useState<MetaResponse>(defaultPage);
   const [userParams, setUserParams] = useState<UserListParams>({ ...defaultPramsList });
   const [isOpen, { open: onOpen, close: onClose }] = useDisclosure(false);
-  const [selected, setSelected] = useState<User>();
+  const [selected, setSelected] = useState<User | null>(null);
   const { authUser } = useAuthStore();
 
-  useEffect(() => {
-    handleGetListUser().then();
-  }, []);
-
-  const handleDelete = async () => {
-    if (selected) {
-      await userService.deleteUser(selected?.id ?? '');
-      handleGetListUser();
+  const handleGetListUser = useCallback(async () => {
+    try {
+      const res = await userService.getList(userParams);
+      setUsers(res.data.data);
+      setMeta(res.data?.meta ?? defaultPage);
+    } catch (error) {
+      console.error('Error fetching users:', error);
     }
-  };
-
-  const handleGetListUser = async () => {
-    const res = await userService.getList(userParams);
-    setUsers(res.data.data);
-    setMeta(res.data?.meta ?? defaultPage);
-  };
+  }, [userParams, userService]);
 
   useEffect(() => {
     handleGetListUser();
-  }, [userParams]);
+  }, [handleGetListUser]);
 
-  const columns: DataTableProps<User>['columns'] = [
-    {
-      accessor: 'firstname',
-      title: 'Tài khoản',
-      render: (user: User) => <UserNameCellTable user={user} />,
-      filter: (
-        <SearchFilter<UserListParams> setParams={setUserParams} searchTermValue={userParams.q} />
-      ),
-      filtering: !!userParams.q,
-    },
-    {
-      accessor: 'role',
-      title: 'Vai trò',
-      render: (user: User) => <RoleBadge role={user?.role} />,
-      filter: (
-        <RoleFilter
-          value={userParams.role}
-          onChange={(value) => setUserParams({ ...userParams, role: value as RoleEnum })}
-        />
-      ),
-      filtering: !!userParams.role,
-    },
-    {
-      accessor: 'created_at',
-      title: 'Ngày tạo',
-      sortable: true,
-      render: (user: User) => (
-        <Text fz="sm">{formatDateString(user?.created_at, 'HH:MM dd/mm/yyyy')}</Text>
-      ),
-    },
-    {
-      accessor: 'id',
-      title: 'Hành động',
-      width: 100,
-      render: (user: User) => (
-        <Menu withArrow width={150} shadow="md">
-          <Menu.Target>
-            <div style={{ cursor: 'pointer', display: 'flex' }}>
-              <Button variant="filled" size="xs">
-                <IconDotsVertical color="white" size={16} />
-              </Button>
-            </div>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Item
-              fw={600}
-              fz="sm"
-              color="blue"
-              variant="filled"
-              component={Link}
-              leftSection={<IconEdit size={16} />}
-              href={userRoute.update(user?.id)}
-            >
-              Chỉnh sửa
-            </Menu.Item>
-            {user?.id !== authUser?.id && (
-              <Menu.Item
-                fw={600}
-                fz="sm"
-                color="red"
-                variant="filled"
-                leftSection={<IconTrash size={16} />}
-                onClick={() => {
-                  setSelected(() => user);
-                  onOpen();
-                }}
-              >
-                Xóa
-              </Menu.Item>
-            )}
-          </Menu.Dropdown>
-        </Menu>
-      ),
-    },
-  ];
+  const handleDelete = useCallback(async () => {
+    if (selected) {
+      try {
+        await userService.deleteUser(selected?.id ?? '');
+        handleGetListUser();
+      } catch (error) {
+        console.error('Error deleting user:', error);
+      }
+    }
+  }, [selected, userService, handleGetListUser]);
+
+  const columns: DataTableProps<User>['columns'] = useMemo(
+    () => [
+      {
+        accessor: 'firstname',
+        title: 'Tài khoản',
+        render: (user: User) => <UserNameCellTable user={user} />,
+        filter: (
+          <SearchFilter<UserListParams> setParams={setUserParams} searchTermValue={userParams.q} />
+        ),
+        filtering: !!userParams.q,
+      },
+      {
+        accessor: 'role',
+        title: 'Vai trò',
+        render: (user: User) => <RoleBadge role={user?.role} />,
+        filter: (
+          <RoleFilter
+            value={userParams.role}
+            onChange={(value) => setUserParams({ ...userParams, role: value as RoleEnum })}
+          />
+        ),
+        filtering: !!userParams.role,
+      },
+      {
+        accessor: 'created_at',
+        title: 'Ngày tạo',
+        sortable: true,
+        render: (user: User) => (
+          <Text fz="sm">{formatDateString(user?.created_at, 'HH:MM dd/mm/yyyy')}</Text>
+        ),
+      },
+      {
+        accessor: 'id',
+        title: 'Hành động',
+        width: 100,
+        render: (user: User) => (
+          <UserActionMenu user={user} onOpen={onOpen} setSelected={setSelected} />
+        ),
+      },
+    ],
+    [authUser?.id, onOpen, setUserParams, userParams.q, userParams.role]
+  );
 
   return (
     <UserPageStyled>
@@ -148,7 +120,6 @@ const UserPage = () => {
               </Button>
             }
           />
-
           <Paper p="md" shadow="md" radius="md">
             <CommonDataTable
               meta={meta}
