3dc1e667cdbd0fba6b3dbea2f61a84ec82b78cd4
chiendd <chiendam1908@gmail.com>
2024-10-05 17:04:23 +0700

    feat: create ui department

--- features/departments/components/Cells/DepartmentActionMenu.tsx
+++ features/departments/components/Cells/DepartmentActionMenu.tsx
@@ -3,7 +3,6 @@ import { IconEdit, IconDotsVertical, IconTrash } from '@tabler/icons-react';
 import Link from 'next/link';
 import { departmentRoute } from '@/routes';
 import { Department } from '@/types';
-import { useAuthStore } from '@/utils/recoil/auth/authState';
 
 interface DepartmentActionMenuProps {
   department: Department;
@@ -15,48 +14,42 @@ const DepartmentActionMenu: React.FC<DepartmentActionMenuProps> = ({
   department,
   onOpen,
   setSelected,
-}) => {
-  const { authUser } = useAuthStore();
-
-  return (
-    <Menu withArrow width={150} shadow="md">
-      <Menu.Target>
-        <div style={{ cursor: 'pointer', display: 'flex' }}>
-          <Button variant="filled" size="xs">
-            <IconDotsVertical color="white" size={16} />
-          </Button>
-        </div>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Item
-          fw={600}
-          fz="sm"
-          color="blue"
-          variant="filled"
-          component={Link}
-          leftSection={<IconEdit size={16} />}
-          href={departmentRoute.update(department?.id)}
-        >
-          Chỉnh sửa
-        </Menu.Item>
-        {department?.id !== authUser?.id && (
-          <Menu.Item
-            fw={600}
-            fz="sm"
-            color="red"
-            variant="filled"
-            leftSection={<IconTrash size={16} />}
-            onClick={() => {
-              setSelected(department);
-              onOpen();
-            }}
-          >
-            Xóa
-          </Menu.Item>
-        )}
-      </Menu.Dropdown>
-    </Menu>
-  );
-};
+}) => (
+  <Menu withArrow width={150} shadow="md">
+    <Menu.Target>
+      <div style={{ cursor: 'pointer', display: 'flex' }}>
+        <Button variant="filled" size="xs">
+          <IconDotsVertical color="white" size={16} />
+        </Button>
+      </div>
+    </Menu.Target>
+    <Menu.Dropdown>
+      <Menu.Item
+        fw={600}
+        fz="sm"
+        color="blue"
+        variant="filled"
+        component={Link}
+        leftSection={<IconEdit size={16} />}
+        href={departmentRoute.update(department?.id)}
+      >
+        Chỉnh sửa
+      </Menu.Item>
+      <Menu.Item
+        fw={600}
+        fz="sm"
+        color="red"
+        variant="filled"
+        leftSection={<IconTrash size={16} />}
+        onClick={() => {
+          setSelected(department);
+          onOpen();
+        }}
+      >
+        Xóa
+      </Menu.Item>
+    </Menu.Dropdown>
+  </Menu>
+);
 
 export default DepartmentActionMenu;
