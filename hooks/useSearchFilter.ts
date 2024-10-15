ff6dc0e996497645699bb374be5099ab66aad350
minhlh <minhlh@haposoft.com>
2024-10-15 09:53:33 +0700

    fix: search filter

--- hooks/useSearchFilter.ts
+++ hooks/useSearchFilter.ts
@@ -2,10 +2,10 @@ import { useCallback, useRef, useState } from 'react';
 
 export const useSearchFilter = <T extends Record<string, any>>(
   setParams: (params: T) => void,
-  setSearchTermValue?: string
+  searchTermValue?: string
 ) => {
-  const [searchTerm, setSearchTerm] = useState<string>(setSearchTermValue ?? '');
-  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>(searchTermValue ?? '');
+  const typingTimeoutRef = useRef<number | NodeJS.Timeout | null>(null);
 
   const updateSearchQuery = useCallback(
     (value: string) => {
@@ -14,9 +14,8 @@ export const useSearchFilter = <T extends Record<string, any>>(
       }
 
       typingTimeoutRef.current = setTimeout(() => {
-        // @ts-ignore
-        setParams((prevParams) => ({ ...prevParams, q: value }));
-      }, 300); // 300ms
+        setParams((prevParams: T) => ({ ...prevParams, q: value }));
+      }, 300); // 300ms debounce
     },
     [setParams]
   );
