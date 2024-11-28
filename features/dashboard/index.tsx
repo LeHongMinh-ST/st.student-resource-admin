00ed3033f6ec04096a7605f6cbfcef626250bb44
minhlh <minhlh@haposoft.com>
2024-11-28 15:36:35 +0700

    feat: fix dashboard

--- features/dashboard/index.tsx
+++ features/dashboard/index.tsx
@@ -2,9 +2,7 @@ import { Container, Stack } from '@mantine/core';
 import { PageHeader } from '@/components';
 import { User } from '@/types';
 import { useAuthStore } from '@/utils/recoil/auth/authState';
-import Role from '@/enums/role.enum';
 import DashboardAdmin from '@/features/dashboard/DashboardAdmin';
-import DashboardTeacher from '@/features/dashboard/DashboardTeacher';
 
 const Dashboard = () => {
   const authState = useAuthStore();
@@ -13,11 +11,7 @@ const Dashboard = () => {
     <Container fluid>
       <Stack gap="lg">
         <PageHeader user={user} title="Welcome!" hasGreetings />
-        {user?.role === Role.Admin ? (
-          <DashboardAdmin user={user} />
-        ) : (
-          <DashboardTeacher user={user} />
-        )}
+        <DashboardAdmin user={user} />
       </Stack>
     </Container>
   );
