6ce31cb3945ee119c4e50030b7a66b76e1fe9968
minhlh <minhlh@haposoft.com>
2024-12-12 09:36:44 +0700

    fix: label

--- constants/sidebarNavigation.ts
+++ constants/sidebarNavigation.ts
@@ -80,25 +80,10 @@ export const sidebarNavigationOfficer: SidebarNavigationProps[] = [
     title: 'Sinh viên',
     links: [
       { label: 'Danh sách sinh viên', icon: IconUsers, link: studentRoute.listCourse },
-      { label: 'Đợt tốt nghiệp', icon: IconSchool, link: '/students-graduation' },
       { label: 'Cảnh báo', icon: IconAlertTriangle, link: '/students-warning' },
       { label: 'Buộc thôi học', icon: IconBan, link: '/students-leave' },
       { label: 'Xác nhận thông tin', icon: IconUserEdit, link: '/students-request' },
       { label: 'Phản ánh', icon: IconMessageQuestion, link: '/students-report' },
-    ],
-  },
-  {
-    title: 'Khảo sát',
-    links: [
-      { label: 'Khảo sát viêc làm', icon: IconListDetails, link: '/form-job' },
-      { label: 'Đánh giá rèn luyện', icon: IconListNumbers, link: '/form-point' },
-    ],
-  },
-  {
-    title: 'Báo cáo - thống kê',
-    links: [
-      { label: 'Tình hình việc làm', icon: IconChartDots, link: '/statistics-job' },
-      { label: 'Báo cáo tổng hợp', icon: IconChartBar, link: '/statistics' },
     ],
   },
   {
@@ -119,7 +104,6 @@ export const sidebarNavigationTeacher: SidebarNavigationProps[] = [
     title: 'Sinh viên',
     links: [
       { label: 'Danh sách sinh viên', icon: IconUsers, link: studentRoute.listCourse },
-      { label: 'Đợt tốt nghiệp', icon: IconSchool, link: '/students-graduation' },
       { label: 'Cảnh báo', icon: IconAlertTriangle, link: '/students-absent' },
       { label: 'Buộc thôi học', icon: IconBan, link: '/students-leave' },
       { label: 'Xác nhận thông tin', icon: IconUserEdit, link: '/students-request' },
